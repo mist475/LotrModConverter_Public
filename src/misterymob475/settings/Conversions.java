b9577ada01dd6ee41b298fc949b0a5117bf98a2b
mist475 <475misterymob@gmail.com>
2022-10-11 20:11:31 +0200

    settings in pojo for better handling

---
+++ src/misterymob475/settings/Conversions.java
@@ -0,0 +1,156 @@
+package misterymob475.settings;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+import java.util.Map;
+
+public class Conversions {
+    @SerializedName("Additional Comments")
+    private final String comments;
+    @SerializedName("AuthorBlacklist")
+    private final List<String> authorBlackList;
+    @SerializedName("BlockEntityMappings")
+    private final Map<String,String> blockEntityMappings;
+    @SerializedName("BlockIdToName")
+    private final Map<String,String> blockIdToName;
+
+    @SerializedName("BlockMappings")
+    private final Map<String,Map<String,BlockMapping>> blockMappings;
+
+    @SerializedName("Colours")
+    private final Map<String,String> colours;
+    @SerializedName("Enchantments")
+    private final Map<String,String> enchantmentIds;
+    @SerializedName("Entities")
+    private final Map<String,String> entities;
+    @SerializedName("Factions")
+    private final Map<String,String> factions;
+    @SerializedName("Items")
+    private final Map<String,List<String>> items;
+    @SerializedName("Mod_mob_ids")
+    private final Map<String,String> lotrModMobIds;
+    @SerializedName("Potions")
+    private final Map<String,Potion> potions;
+    @SerializedName("Regions")
+    private final Map<String,String> regions;
+
+    @SerializedName("Settings")
+    private final Settings settings;
+    @SerializedName("Text_Formatting")
+    private final Map<String, Object> textFormatting;
+    @SerializedName("TitleBlacklist")
+    private final List<String> titleBlacklist;
+    @SerializedName("Vanilla_mob_ids")
+    private final Map<String,String> vanillaMobIds;
+    @SerializedName("Waypoints")
+    private final Map<String,String> waypoints;
+
+    public Conversions(String comments, List<String> authorBlackList, Map<String, String> blockEntityMappings, Map<String, String> blockIdToName, Map<String, Map<String, BlockMapping>> blockMappings, Map<String, String> colours, Map<String, String> enchantmentIds, Map<String, String> entities, Map<String, String> factions, Map<String, List<String>> items, Map<String, String> lotrModMobIds, Map<String, Potion> potions, Map<String, String> regions, Settings settings, Map<String, Object> textFormatting, List<String> titleBlacklist, Map<String, String> vanillaMobIds, Map<String, String> waypoints) {
+        this.comments = comments;
+        this.authorBlackList = authorBlackList;
+        this.blockEntityMappings = blockEntityMappings;
+        this.blockIdToName = blockIdToName;
+        this.blockMappings = blockMappings;
+        this.colours = colours;
+        this.enchantmentIds = enchantmentIds;
+        this.entities = entities;
+        this.factions = factions;
+        this.items = items;
+        this.lotrModMobIds = lotrModMobIds;
+        this.potions = potions;
+        this.regions = regions;
+        this.settings = settings;
+        this.textFormatting = textFormatting;
+        this.titleBlacklist = titleBlacklist;
+        this.vanillaMobIds = vanillaMobIds;
+        this.waypoints = waypoints;
+    }
+
+    public String getComments() {
+        return comments;
+    }
+
+    public List<String> getAuthorBlackList() {
+        return authorBlackList;
+    }
+
+    public List<String> getTitleBlacklist() {
+        return titleBlacklist;
+    }
+
+    public Map<String, List<String>> getItems() {
+        return items;
+    }
+
+    public Map<String, Map<String, BlockMapping>> getBlockMappings() {
+        return blockMappings;
+    }
+
+    public Map<String, Object> getTextFormatting() {
+        return textFormatting;
+    }
+
+    public Map<String, Potion> getPotions() {
+        return potions;
+    }
+
+    public Map<String, String> getBlockEntityMappings() {
+        return blockEntityMappings;
+    }
+
+    public Map<String, String> getBlockIdToName() {
+        return blockIdToName;
+    }
+
+    public Map<String, String> getColours() {
+        return colours;
+    }
+
+    public Map<String, String> getEnchantmentIds() {
+        return enchantmentIds;
+    }
+
+    public Map<String, String> getEntities() {
+        return entities;
+    }
+
+    public Map<String, String> getFactions() {
+        return factions;
+    }
+
+    public Map<String, String> getLotrModMobIds() {
+        return lotrModMobIds;
+    }
+
+    public Map<String, String> getRegions() {
+        return regions;
+    }
+
+    public Map<String, String> getVanillaMobIds() {
+        return vanillaMobIds;
+    }
+
+    public Map<String, String> getWaypoints() {
+        return waypoints;
+    }
+
+    public Settings getSettings() {
+        return settings;
+    }
+
+    private class Potion {
+        @SerializedName("Name")
+        public String name;
+        @SerializedName("Splash")
+        public boolean splash;
+
+        public String getName() {
+            return name;
+        }
+
+        public boolean isSplash() {
+            return splash;
+        }
+    }
+}
---
+++ src/misterymob475/settings/Settings.java
@@ -0,0 +1,44 @@
+package misterymob475.settings;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Settings {
+    @SerializedName("Debug Messages")
+    private final int debugMessages;
+    @SerializedName("Cache debug messages")
+    private final boolean cacheDebugMessages;
+    @SerializedName("Recursion Depth")
+    private final int itemRecursionDepth;
+    @SerializedName("Creative Mode spawn")
+    private final boolean creativeSpawn;
+    @SerializedName("Comments")
+    private final String comments;
+
+    public Settings(int debug_messages, boolean cache_debug_messages, int recursion_depth, boolean creative_spawn, String comments) {
+        this.debugMessages = debug_messages;
+        this.cacheDebugMessages = cache_debug_messages;
+        this.itemRecursionDepth = recursion_depth;
+        this.creativeSpawn = creative_spawn;
+        this.comments = comments;
+    }
+
+    public boolean isCacheDebugMessages() {
+        return cacheDebugMessages;
+    }
+
+    public boolean isCreativeSpawn() {
+        return creativeSpawn;
+    }
+
+    public int getDebugMessages() {
+        return debugMessages;
+    }
+
+    public int getItemRecursionDepth() {
+        return itemRecursionDepth;
+    }
+
+    public String getComments() {
+        return comments;
+    }
+}
